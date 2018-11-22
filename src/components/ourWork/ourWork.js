d16eeb05762ca1540f42356b86afbb0ade935146
drkimpatrick <dr.kimanje2patrick2@gmail.com>
2018-11-22 23:42:12 +0300

    add spaces between our work columns

--- src/components/ourWork/ourWork.js
+++ src/components/ourWork/ourWork.js
@@ -4,37 +4,37 @@ import '../../styles/ourWork/ourWork.css';
 const OurWork = () => {
   return <div className="container">
             <div className="row">
+                <div className="col-xs-12 text-center"><h1 className="heading"><u>OUR WORK</u></h1></div>
+            </div>
+            <div className="row">
                 <div className="col-xs-8 col-xs-offset-2 text-center">
-                <div className="row">
-                    <div className="col-xs-12 text-center"><h1 className="heading"><u>OUR WORK</u></h1></div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-12">
-                        <div className="row work-row-margin">
-                            <div className="col-xs-12 col-sm-4">
-                                <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <div className="row">
+                                <div className="col-xs-12 col-sm-4 col-margin-bottom">
+                                    <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                                </div>
+                                <div className="col-xs-12 col-sm-4 col-margin-bottom">
+                                    <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                                </div>
+                                <div className="col-xs-12 col-sm-4 col-margin-bottom">
+                                    <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                                </div>
                             </div>
-                            <div className="col-xs-12 col-sm-4">
-                                <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
-                            </div>
-                            <div className="col-xs-12 col-sm-4">
-                                <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
-                            </div>
-                        </div>
 
-                        <div className="row work-row-margin">
-                            <div className="col-xs-12 col-sm-4">
-                                <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
-                            </div>
-                            <div className="col-xs-12 col-sm-4">
-                                <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
-                            </div>
-                            <div className="col-xs-12 col-sm-4">
-                                <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                            <div className="row work-row-margin">
+                                <div className="col-xs-12 col-sm-4 col-margin-bottom">
+                                    <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                                </div>
+                                <div className="col-xs-12 col-sm-4 col-margin-bottom">
+                                    <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                                </div>
+                                <div className="col-xs-12 col-sm-4 col-margin-bottom">
+                                    <img src={require("../../static/img2.jpg")} alt={"image"} className="img-responsive" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
          </div>
